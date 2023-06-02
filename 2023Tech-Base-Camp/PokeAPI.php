01c6b56daa874b2c0e94631dc49e7a60a9e9712f
ShuTanaka <s-tanaka-a@usen-next.jp>
2023-06-02 16:54:06 +0900

    first commit

---
+++ 2023Tech-Base-Camp/PokeAPI.php
@@ -0,0 +1,14 @@
+<?php
+// PokeAP1 のデータを取得する ( id = 11 から 29 のポケモンのデータ ) * /
+$url = 'https://pokeapi.co/api/v2/pokemon/1/?limit=10$offset=0';
+$response = file_get_contents($url);
+
+// レスポンスデータは JSON 形式なので 、デコードして連想配列にする
+$data = json_decode($response, true);
+
+// 取得結果をループさせてポケモンの名前を表示する
+print("<pre>");
+foreach ($data['results'] as $key => $value) {
+    var_dump($value['name']);
+}
+print("</pre>");
