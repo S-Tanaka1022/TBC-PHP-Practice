0b99a54d5d4537ef875b84fe03aa4cace746d4c6
ShuTanaka <s-tanaka-a@usen-next.jp>
2023-06-02 14:43:54 +0900

    delete

---
+++ 2023Tech-Base-Camp/delete.php
@@ -0,0 +1,23 @@
+<?php
+
+$dsn = 'mysql:host=localhost; dbname=booksample; charset=utf8';
+$user = 'root';
+$pass = 'root';
+
+try {
+    $dbh = new PDO($dsn, $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if ($dbh == null) {
+        echo "接続に失敗しました。";
+    } else {
+
+        # SQL文を定める
+        $SQL = <<<_SQL_
+        DROP TABLE booksample
+_SQL_;
+    }
+} catch (PDOException $e) {
+    echo "エラー内容：" . $e->getMessage();
+    die();
+}
+$dbh = null;
